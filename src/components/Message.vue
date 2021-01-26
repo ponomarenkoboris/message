cbefc6d3ef21426ba9e26b652daa51b3d60b7875
Boris Ponomarenko <borisponomarenko@gmail.com>
2021-01-26 22:24:53 +0300

    init

---
+++ src/components/Message.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <p class="text-xs-center">{{ text }}</p>
+      <hr>
+    </div>
+    <div class="wrap">
+      <div class="message">
+        <small><strong>{{ name }}</strong></small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  name: "Message",
+  setup() {
+    const name = ref('msbk');
+    const text = ref('hellow nsdk');
+
+    return {
+      name, text
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Message msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Message from "@/components/Message.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Message
+  }
+};
+</script>
